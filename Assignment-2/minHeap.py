e7862a52e54fc036b77acf4953521f07f25f6b68
amanlp7 <ak547276@gmail.com>
2020-06-30 16:20:47 +0530

    Added code for minimum heap implementation.

--- Assignment-2/minHeap.py
+++ Assignment-2/minHeap.py
@@ -3,8 +3,7 @@
 import sys
 
 # Class to create a heap data structure
-
-class minHeap():
+class minimumHeap():
 
 
     def __init__(self, maxSize):
@@ -64,6 +63,93 @@ class minHeap():
     '''
     def swap(self, first, second):
         self.heap[first], self.heap
+
+
+    # Function to heapify the node at position
+    '''
+    Input: Postion of the node
+    Output: Return new minimum heap
+    '''
+    def minHeapify(self, position):
+
+        # If the node is non-leaf node and greater than any of its children
+        if not self.isLeaf(position):
+            if (
+                (self.heap[position] > self.heap[self.leftChild(position)]) or 
+                (self.heap[position] > self.heap[self.rightChild(position)])
+                ): 
+
+                # Swap with left child and heapify the left child
+                if self.heap[self.leftChild(position)] < self.heap[self.rightChild(position)]:
+                    self.swap(position, self.leftChild(position))
+                    self.minHeapify(self.leftChild(position))
+
+                # Swap with the right child and heapify the right child
+                else:
+                    self.swap(position, self.rightChild(position))
+                    self.minHeapify(self.rightChild(position))
+
+
+    # Function to insert new node in the heap
+    '''
+    Input: Element to be inserted
+    '''
+    def insertElement(self, element):
+
+        if self.size > self.maxSize:
+            return
+        self.size += 1
+        self.heap[self.size] = element
+        currentPosition = self.size
+
+        while self.heap[currentPosition] < self.heap[self.parent(currentPosition)]:
+            self.swap(currentPosition, self.parent(currentPosition))
+            currentPosition = self.parent(currentPosition)
+
+
+    # Function to print contents of the heap
+    def printHeap(self):
+        for i in range(1, (self.size//2)+1):
+            print(f"PARENT : {self.heap[i]} LEFT CHILD : {self.heap[2*i]} RIGHT CHILD : {self.heap[2*i + 1]}")
+
+
+    # Function to build minimum heap using minHeapify function
+    def buildMinHeap(self):
+        for position in range(self.size//2, 0, -1):
+            self.minHeapify(position)
+
+    
+    # Function to remove and return the minimum element from the heap
+    def removeMinElement(self):
+        minElement = self.heap[self.FRONT]
+        self.heap[self.FRONT] = self.heap[self.size]
+        self.size -= 1
+        self.minHeapify(self.FRONT)
+
+        return minElement
+
+
+##-------------------------------------------------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+
+    print("\nThe minimum heap is .....\n")
+    heap1 = minimumHeap(15)
+    heap1.insertElement(5) 
+    heap1.insertElement(3) 
+    heap1.insertElement(17) 
+    heap1.insertElement(10) 
+    heap1.insertElement(84) 
+    heap1.insertElement(19) 
+    heap1.insertElement(6) 
+    heap1.insertElement(22) 
+    heap1.insertElement(9) 
+    heap1.buildMinHeap()
+    heap1.printHeap()
+
+
+
     
 
     
